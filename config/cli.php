0f409b10e845830b90d9d1989dde82acd33d832b
Francois Lajoie <snake386@hotmail.com>
2017-07-12 22:55:00 -0400

    updated

--- config/cli.php
+++ config/cli.php
@@ -1,13 +1,14 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| Cli Application base configurations
+| Cli Application configurations overload for development environment
 |--------------------------------------------------------------------------
 */
 return [
     'php' => [
-        'display_errors'         => 0,
-        'display_startup_errors' => 0,
+        'display_errors'         => 1,
+        'display_startup_errors' => 1,
         'date.timezone'          => 'America/Toronto',
     ],
+    'climber_cmd_prefix' => 'php climber'
 ];
