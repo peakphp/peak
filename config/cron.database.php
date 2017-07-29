1b7dbedbe4cfc71903d437ac7429098882db1713
Francois Lajoie <snake386@hotmail.com>
2017-07-29 16:13:32 -0400

    updated

--- config/cron.database.php
+++ config/cron.database.php
@@ -2,17 +2,21 @@
 
 return [
     //sqlite
-    'crondb' => [
-        'driver' => 'pdo_sqlite',
-        'path' => __DIR__.'/../database/cron.sqlite'
+    'cron' => [
+        'db' => [
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__.'/../database/cron.sqlite'
+        ]
     ],
 
     //mysql
-    /*'crondb' => [
-       'driver' => 'pdo_mysql',
-       'host' => 'localhost',
-       'dbname' => 'crondatabase',
-       'user' => 'root',
-       'password' => '',
-   ],*/
+    /*'cron' => [
+        'db' => [
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'dbname' => 'testcron',
+            'user' => 'root',
+            'password' => '',
+        ]
+    ],*/
 ];
