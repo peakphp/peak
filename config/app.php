8dafd3f35638da57e7befce3b1f053330f24e881
Francois Lajoie <snake386@hotmail.com>
2017-08-08 11:14:31 -0400

    updated

--- config/app.php
+++ config/app.php
@@ -5,21 +5,34 @@
 |--------------------------------------------------------------------------
 */
 return [
+    // General php setting(s) overflow, in case you can't tweak directly your php.ini.
     'php' => [
         'display_errors'         => 0,
         'display_startup_errors' => 0,
         'date.timezone'          => 'America/New_York',
     ],
+
+    // Define default workflow of the front.
     'front' => [
         'default_controller' => 'index',
         'error_controller'   => 'error',
     ],
+
+    // Prepare the view engine and the layout.
     'view' => [
         'engine'    => 'Layouts',
         'useLayout' => 'default',
     ],
-    'routes' => [ //custom route(s) ...
-        //example:
+
+    // Let the routing system guess route based on the request uri.
+    'auto_routing' => 1,
+
+    // Define custom route(s).
+    // When "auto_routing" is disabled, only defined route(s) below will be resolved.
+    // Regardless of "auto_routing" value, if the request uri is empty, the front 
+    // "default_controller" will be use
+    'routes' => [
+        // example:
         //'user/{id}:num | user/profile',
     ]
 ];
--- config/cli.php
+++ config/cli.php
@@ -1,21 +1,24 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| Cli Application configurations overload for development environment
+| Cli application base configurations
 |--------------------------------------------------------------------------
 */
 return [
+    // General php setting(s) overflow, in case you can't tweak directly your php.ini.
     'php' => [
         'display_errors'         => 1,
         'display_startup_errors' => 1,
         'date.timezone'          => 'America/Toronto',
     ],
     
+    // Cron specific settings.
     'cron' => [
         'processor_prefix' => 'php climber',
         'cmd_prefix' => 'cron'
     ],
 
+    // Register your commands here (support Dependency Injection).
     'commands' => [
         \Cli\Commands\ExampleCommand::class
     ]
