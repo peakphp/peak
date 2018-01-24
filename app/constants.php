49f9e9cc3db4f740465a4af8522b9ca65efefbb4
Francois Lajoie <snake386@hotmail.com>
2018-01-24 12:14:01 -0500

    added

---
+++ app/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application constants
+|--------------------------------------------------------------------------
+*/
+
+define('CACHE_PATH', realpath(__DIR__.'/../cache'));
+define('CONFIG_PATH', realpath(__DIR__.'/../config'));
+define('LOG_PATH', realpath(__DIR__.'/../logs'));
