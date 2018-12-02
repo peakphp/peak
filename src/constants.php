4131204892d8b0cb34c8d51ab263c41baba26ad3
Francois Lajoie <snake386@hotmail.com>
2018-12-02 17:59:11 -0500

    added /tmp folder update src folder structure

--- src/constants.php
+++ src/constants.php
@@ -2,10 +2,11 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application constants
+| Project constants
 |--------------------------------------------------------------------------
 */
 
 define('CACHE_PATH', realpath(__DIR__ . '/../cache'));
 define('CONFIG_PATH', realpath(__DIR__ . '/../config'));
 define('LOG_PATH', realpath(__DIR__ . '/../logs'));
+define('TMP_PATH', realpath(__DIR__ . '/../tmp'));
