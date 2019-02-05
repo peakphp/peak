4446909b88891965a85db3e81edc1bb1832efc06
Franck <snake386@hotmail.com>
2019-02-04 23:17:16 -0500

    updated

--- public/index.php
+++ public/index.php
@@ -1,70 +1,76 @@
 <?php
 
-require '../vendor/autoload.php';
 
-use Peak\Backpack\AppBuilder;
-use Peak\Backpack\ConfigLoader;
-use Peak\Http\Response\Emitter;
-use Zend\Diactoros\ServerRequestFactory;
 
-try {
-    // create a cached configuration
-    /** @var \Peak\Config\Config $config */
-    $config = (new ConfigLoader())
-        ->setCache(CACHE_PATH, 'app-config', 10)
-        ->load([
-            PROJECT_PATH.'/.env',
-            CONFIG_PATH . '/app.yml',
-        ]);
+namespace {
 
-    // create main application
-    /** @var \Peak\Bedrock\Application\Application $app */
-    $app = (new AppBuilder())
-        ->setEnv($config->ENV ?? 'production')
-        ->setProps($config)
-        ->build();
+    use App\Controller\HomeController;
+    use App\Controller\NotFoundController;
+    use Peak\Backpack\AppBuilder;
+    use Peak\Backpack\Bootstrap\Routing;
+    use Peak\Backpack\Bootstrap\Session;
+    use Peak\Backpack\ConfigLoader;
+    use Peak\Http\Response\Emitter;
+    use Zend\Diactoros\ServerRequestFactory;
 
-    // Add application instance to the container
-    $app->getContainer()->set($app, 'MyApp');
+    require '../vendor/autoload.php';
 
-    // Bootstrap stuff
-    $app->bootstrap([
-        \Peak\Backpack\Bootstrap\PhpSettings::class,
-        \Peak\Backpack\Bootstrap\Session::class,
-        \Peak\Backpack\Bootstrap\Routing::class,
-    ]);
+    try {
+        // create a cached configuration
+        $config = (new ConfigLoader())
+            ->setCache(CACHE_PATH, 'app-config', 60)
+            ->load([
+                ['env' => getenv()],
+                CONFIG_PATH . '/app.yml',
+            ]);
 
-    // Register a route
-    $app->get('/',\App\Controller\HomeController::class);
+        // create main application
+        /** @var \Peak\Bedrock\Application\Application $app */
+        $app = (new AppBuilder())
+            ->setEnv($config->get('env.ENV', 'production'))
+            ->setProps($config)
+            ->addToContainerAfterBuild()
+            ->build();
 
-    // Stack a 404 handler at the end of app stack
-    $app->stack(\App\Controller\NotFoundController::class);
+        $app
+            // bootstrap stuff
+            ->bootstrap([
+                Session::class,
+                Routing::class,
+            ])
+            // Register a home route
+            ->get('/', HomeController::class)
 
-    // Run app stack with current server request
-    $app->run(ServerRequestFactory::fromGlobals(), new Emitter());
+            // Stack a 404 handler at the end of application stack
+            ->stack(NotFoundController::class)
 
-} catch(\Exception $e) {
+            // Run application stack with current server request
+            ->run(ServerRequestFactory::fromGlobals(), new Emitter());
 
-    // Create an error application
-    $errorApp = (new AppBuilder())
-        ->setProps(new \Peak\Collection\PropertiesBag([
-            'app' => $app ?? null
-        ]))
-        ->setEnv($config->ENV ?? 'production')
-        ->build();
+    } catch (\Exception $e) {
 
-    // Stack and run without a server request
-    $errorApp
-        ->stack(new \App\Controller\ErrorController($errorApp, $e))
-        ->runDry(new Emitter());
-}
+        echo $e->getMessage();
+        // Create an error application
+        $errorApp = (new AppBuilder())
+            ->setProps(new \Peak\Collection\PropertiesBag([
+                'app' => $app ?? null
+            ]))
+            ->setEnv($config->get('env.ENV', 'production'))
+            ->build();
+
+        // Stack and run without a server request
+        $errorApp
+            ->stack(new \App\Controller\ErrorController($errorApp, $e))
+            ->runDry(new Emitter());
+    }
 
 // Create a DebugBar with dev env
-if (isset($app) && $app->getKernel()->getEnv() === 'dev') {
-    $debugBar = new \Peak\DebugBar\DebugBar($app->getContainer());
-    $debugBar
-        ->addDefaultModules()
-        ->addModule(new \Peak\Backpack\DebugBar\AppConfig\AppConfig($app))
-        ->addModule(new \Peak\Backpack\DebugBar\AppContainer\AppContainer($app));
-    echo $debugBar->render();
+    if (isset($app) && $app->getKernel()->getEnv() === 'dev') {
+        $debugBar = new \Peak\DebugBar\DebugBar($app->getContainer());
+        $debugBar
+            ->addDefaultModules()
+            ->addModule(new \Peak\Backpack\DebugBar\AppConfig\AppConfig($app))
+            ->addModule(new \Peak\Backpack\DebugBar\AppContainer\AppContainer($app));
+        echo $debugBar->render();
+    }
 }
